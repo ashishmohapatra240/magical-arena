aa33da4f98b703ba2ed317cf4149d43015e03447
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2024-07-10 13:46:25 +0530

    Add attack logic in Arena

--- src/arena.js
+++ src/arena.js
@@ -5,6 +5,33 @@ class Arena {
     this.currentPlayer = player1.health > player2.health ? player2 : player1;
     this.currentDefender = player1.health > player2.health ? player1 : player2;
   }
+
+  fight() {
+    while (!this.isGameOver) {
+      this.attack(this.currentPlayer, this.currentDefender);
+      this.switchPlayers(this.currentPlayer, this.currentDefender);
+    }
+    return this.getWinner();
+  }
+  attack(attacker, defender) {
+    const attackRoll = attacker.rollDice();
+    const defendRoll = defender.rollDice();
+    const damage =
+      attacker.attack * attackRoll - defender.strength * defendRoll;
+    if (damage > 0) {
+      defender.takeDamage(damage);
+    }
+  }
+
+  switchPlayers(player1, player2) {
+    [player1, player2] = [player2, player1];
+  }
+
+  getWinner() {
+    return this.player1.health > this.player2.health
+      ? this.player1
+      : this.player2;
+  }
 }
 
 module.export = Arena;
