94c9569428ecfd0c25d94b4e236439e9368fcd1a
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2024-07-10 13:42:36 +0530

    Implement rollDice and takeDamage methods for the player

--- src/player.js
+++ src/player.js
@@ -4,6 +4,14 @@ class Player {
     this.strength;
     this.attack;
   }
+  rollBack() {
+    Math.floor(Math.random() * 6 + 1);
+  }
+
+  takeDamage(damage) {
+    this.health -= damage;
+    if (this.health < 0) this.health = 0;
+  }
 }
 
 module.export = Player;
